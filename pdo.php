1604f444d3dc23f5a6814340c5ee91bfa82be6b2
hunghang123 <96190778+hunghang123@users.noreply.github.com>
2021-12-16 14:54:25 +0700

    Add files via upload

---
+++ pdo.php
@@ -0,0 +1,7 @@
+<?php 
+$host ="localhost:3306";
+$dbName="bookstore";
+$userName="root";
+$password="";
+$objPDO = new PDO("mysql:host=$host; dbname=$dbName", $userName, $password);
+$objPDO->query('set names utf8');
